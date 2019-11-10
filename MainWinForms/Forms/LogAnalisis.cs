ea617015923150326e0dd1eb2154ba2c7cdb9545
Dima <Dima@DIMA-PC>
2019-11-10 19:03:26 +0200

    Add Windows Forms for Type Analisis? Logs? Patient

---
+++ MainWinForms/Forms/LogAnalisis.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KDLAnalize.MainWinForms.Forms
+{
+    public partial class LogAnalisis : Form
+    {
+        public LogAnalisis()
+        {
+            InitializeComponent();
+        }
+    }
+}
